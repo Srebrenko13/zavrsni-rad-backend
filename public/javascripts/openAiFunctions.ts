847f49b7d71a97401469e9ee1773faa32f48c1eb
Renato Malbašić <renato.malbasic@fer.hr>
2024-05-27 17:23:43 +0200

    created functions for generating nodes of the game, working properly

--- public/javascripts/openAiFunctions.ts
+++ public/javascripts/openAiFunctions.ts
@@ -5,12 +5,12 @@ import * as dotenv from 'dotenv';
 import {Utils} from "./utils";
 
 dotenv.config();
-const utils = new Utils();
 const openai = new OpenAI({
         apiKey: process.env.API_KEYv2,
         organization: process.env.ORG,
     }
 );
+const topic = "futuristic city on another planet";
 
 // create message array to store history in current conversation with our bot
 const history: ChatCompletionMessageParam[] = [];
@@ -20,78 +20,93 @@ const userInterface = readline.createInterface({
     output: process.stdout
 });
 
-// configure bot about what we want from him and how to return/output the response in this case JSON file with
-// specific fields, like story, description, choices, depth etc.
-const systemMessage : ChatCompletionMessageParam = {
-    role: 'system',
-    content: utils.systemSetup(),
-};
-history.push(systemMessage);
+/* configure bot about what we want from him and how to return/output the response in this case JSON file with
+   specific fields, like story, description, choices, depth etc. */
+async function setupSystemSettings(ai: OpenAI, messages: ChatCompletionMessageParam[]){
+    // generate prompt and save it to chat history
+    const systemMessage : ChatCompletionMessageParam = {
+        role: 'system',
+        content: Utils.systemSetup(),
+    };
+    messages.push(systemMessage);
 
-openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
-    response_format: {"type" : "json_object"},
-    messages: history
-});
-
-// set starting prompt for the story theme
-
-userInterface.question('[bot]: Please write desired topic/description for the game',
-    async (topic) => {
-    const startMessage: ChatCompletionMessageParam = {
-        role: 'user',
-        content: topic
-    }
-    history.push(startMessage);
-
-    const story = await openai.chat.completions.create({
+    // call api for response
+    const answer = await ai.chat.completions.create({
         model: 'gpt-3.5-turbo',
         response_format: {"type" : "json_object"},
         messages: history
     });
 
-    console.log(String(story.choices[0]));
-    console.log("[bot]: " + String(story.choices[0].message));
+    // save response to chat history
+    if(answer){
+        messages.push({
+            role: answer.choices[0].message.role,
+            content: answer.choices[0].message.content
+        })
+    }
+}
 
-    userInterface.close();
-});
+async function sendPrompt(ai: OpenAI, message: string, messages: ChatCompletionMessageParam[]) : Promise<boolean>{
+    // generate topic message and save it to message history
+    const prompt: ChatCompletionMessageParam = {
+        role: 'user',
+        content: message
+    }
+    messages.push(prompt);
 
-// set prompt for user
-userInterface.setPrompt(`[bot]: Please choose one of the paths!`);
-userInterface.prompt();
+    // call api for response
+    const story = await ai.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        response_format: {"type" : "json_object"},
+        messages: messages
+    });
+
+    // save response to chat history
+    if(story){
+        messages.push({
+            role: story.choices[0].message.role,
+            content: story.choices[0].message.content
+        })
+    }
+    return printNode(story);
+}
+
+ async function start(){
+    await setupSystemSettings(openai, history);
+    await sendPrompt(openai, topic, history);
+
+    // set up user prompt and push it
+    userInterface.setPrompt(`[bot]: Please choose one of the paths!`);
+    userInterface.prompt();
+}
+
+function printNode(input: OpenAI.Chat.Completions.ChatCompletion) : boolean{
+    const json = JSON.parse(<string>input.choices[0].message.content);
+    console.log(json.story);
+    console.log(json.gameEnded);
+    if(json.gameEnded == false){
+        console.log("OPTIONS:");
+        console.log("1: " + json.firstOpt);
+        console.log("2: " + json.secondOpt);
+        console.log("3: " + json.thirdOpt);
+        return false;
+    }
+    else return true;
+}
 
 // handle user input, process it and return the answer
 userInterface.on('line', async (input) => {
     // create request message and add it to history array
-    const requestMessage: ChatCompletionMessageParam = {
-        role: 'user',
-        content: input
-    }
-    history.push(requestMessage);
-
-    // call OpenAI API to generate response
-    const completion = await openai.chat.completions.create({
-        model: 'gpt-3.5-turbo',
-        response_format: {"type" : "json_object"},
-        messages: history
-    });
-
-    // display response to the user
-    const responseMessage = completion.choices[0].message;
-    if(responseMessage){
-        console.log(String(completion.choices[0]));
-        console.log('[bot]: ' + String(responseMessage.content));
-        history.push({
-            role: responseMessage.role,
-            content: responseMessage.content
-        })
-    }
-
-    // prompt user for next message
-    userInterface.prompt();
+    const status = await sendPrompt(openai, input, history);
+    // prompt user for next answer
+    if(!status) userInterface.prompt();
+    else userInterface.close();
 });
 
 // handle program exit
 userInterface.on('close', () => {
     console.log('[bot]: Thank you for using our bot!');
 });
+
+// setup system settings and generate start of the story with options and first prompt
+start();
--- public/javascripts/utils.ts
+++ public/javascripts/utils.ts
@@ -1,11 +1,12 @@
 export class Utils{
-    systemSetup(){
-        return `
-        I would like you to create nonlinear storytelling game with 5 chapters story based 
-        on a given theme or short description. I need you to generate answer as JSON object 
-        with next fields: "story" (a short story for a given input), "description" (one 
-        sentence description of story), "1", "2", "3" (three possible choices for next
-        chapter of the game).
-    `
+    static systemSetup(){
+        return `I would like you to create nonlinear storytelling game with 5 chapters story based 
+on a given theme or short description. The story MUST end at the 5 chapter or before if player picks
+ option that ends the story. I need you to generate answer as JSON object with next fields: 
+"story" (the story itself, it should be around one paragraph long), "description" (one sentence
+ description of story), "firstOpt", "secondOpt", "thirdOpt" (three possible choices for next chapter 
+ of the game) and "gameEnded" (boolean which marks if story is done, should be true when 5th chapter is 
+ reached). Also please format JSON in proper way so I can parse it later in my code. Some choices can 
+ lead to early ending of the game.`
     }
 }
